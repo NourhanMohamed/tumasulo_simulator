8ffbe2cca1b17bad24b77b57b7fe5b3fca199db8
Nourhan Mohamed <nour@Nourhans-MacBook-Pro.local>
2013-12-24 13:59:52 +0200

    added a common data bus structure

---
+++ src/simulator/CommonDataBus.java
@@ -0,0 +1,19 @@
+package simulator;
+
+public class CommonDataBus {
+	private short data;
+	private String source;
+	
+	public short getData() {
+		return data;
+	}
+	public void setData(short data) {
+		this.data = data;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+}
