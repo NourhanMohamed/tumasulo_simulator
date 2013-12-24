a6f3a5c3351dab0f0a8773294b5c4b71cb7799a2
Nourhan Mohamed <nourhan.abdeltawab@gmail.com>
2013-12-24 14:10:58 +0200

    added initial structure for Functional Units

---
+++ src/simulator/FunctionalUnit.java
@@ -0,0 +1,19 @@
+package simulator;
+
+public class FunctionalUnit {
+	Station[] rs;
+	boolean busy;
+	
+	public FunctionalUnit(int stationCount){
+		rs = new Station[stationCount];
+	}
+	
+	public int findPendingRS() {
+		//TODO
+		return -1;
+	}
+	
+	public void updateRS() {
+		
+	}
+}
